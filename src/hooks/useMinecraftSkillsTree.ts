3a33b49b9b85d4a8450885efa681b34aaa92a7b6
gitrubio <carloviloria0@gmail.com>
2025-02-16 16:47:02 -0500

    Update favicon and enhance CORS support in API fetch

--- src/hooks/useMinecraftSkillsTree.ts
+++ src/hooks/useMinecraftSkillsTree.ts
@@ -16,7 +16,7 @@ export const useMinecraftSkillTree = () => {
 
       dispatch(setLoading(true));
       
-      const response = await fetch(url);
+      const response = await fetch(url,{mode: 'cors'});
       const data = await response.json();
     
       const normalizedData = normalizeTree(data);
